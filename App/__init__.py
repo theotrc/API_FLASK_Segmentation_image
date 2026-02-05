057e43fc64f0e52f11e57bb719bbbc158c29cca8
theotrc <theotricot12@gmail.com>
2026-02-05 13:52:54 +0100

    base

--- App/__init__.py
+++ App/__init__.py
@@ -1,7 +1,5 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from App.utils import download_model
 
 
@@ -15,21 +13,7 @@ print("Model downloaded to:", model_path)
 
 # During testing we avoid importing heavy TensorFlow packages and loading
 # the model at import time. CI/tests should set `TESTING=1` to use a stub.
-if os.environ.get('TESTING') == '1':
-	class _DummyModel:
-		def predict(self, texts):
-			import numpy as _np
-			return _np.array([[0.9, 0.1]])
 
-	bert_model = _DummyModel()
-else:
-	import tensorflow as tf
-	import tensorflow_text
-	import tf_keras
-
-	
-	print("Loading model from:", model_path)
-	bert_model = tf_keras.models.load_model(model_path)
 
 from App import views
 
--- App/utils.py
+++ App/utils.py
@@ -12,32 +12,6 @@ from dotenv import load_dotenv
 
 
 
-
-
-class TextTransformer:
-    
-    def __init__(self):
-        nltk.download('stopwords'); nltk.download('wordnet')
-        self.stop = set(stopwords.words('english'))
-        self.stop.add("u")
-        self.lemmatizer = WordNetLemmatizer()
-        with open('App/models/tfidf_vectorizer.pkl', 'rb') as f:
-            self.tfidf = pickle.load(f)
-            
-    def transform(self, texts):
-        
-        return self.tfidf.transform(texts)
-    
-    def clean_text(self, s):
-        s = (s or "").lower()
-        s = re.sub(r"http\S+|www\S+","", s)
-        s = re.sub(r"[^a-z0-9\sàâéèêçùôî\-]", " ", s)
-        tokens = [t for t in s.split() if t not in self.stop]
-        tokens = [self.lemmatizer.lemmatize(t) for t in tokens]
-        return " ".join(tokens)
-
-
-
 def download_model():
     load_dotenv(override=True)
     
